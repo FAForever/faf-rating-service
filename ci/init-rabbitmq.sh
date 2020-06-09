910f9712054244de27105f977c10bedaa74f1a8c
Clemens Borys <clemens@borys.dk>
2020-06-09 18:55:40 +0200

    simpler rabbitMQ startup

--- ci/init-rabbitmq.sh
+++ ci/init-rabbitmq.sh
@@ -2,8 +2,8 @@
 
 MAX_WAIT=60 # max. 1 minute waiting time in loop before timeout
 
-source faf-rabbitmq.env
-docker run --rm -d -p 5672:5672 --env-file faf-rabbitmq.env --name faf-rabbitmq-test rabbitmq:3.8.2-management-alpine
+source ci/faf-rabbitmq.env
+docker run --rm -d -p 5672:5672 --env-file ci/faf-rabbitmq.env --name faf-rabbitmq-test rabbitmq:3.8.2-management-alpine
 
 # Create RabbitMQ users
 docker exec faf-rabbitmq-test rabbitmqctl wait --timeout ${MAX_WAIT} "${RABBITMQ_PID_FILE}"
